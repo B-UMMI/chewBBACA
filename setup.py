2b28b15fb86c64cf5e80d20e7ba8692fd29ac9a8
mickaelsilva <mickaelsilva@live.com.pt>
2018-01-29 14:59:51 +0000

    added install_requires to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from distutils.core import setup
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'chewBBACA',
   packages = ['chewBBACA'],
@@ -9,6 +22,9 @@ setup(
   url = 'https://github.com/B-UMMI/chewBBACA', 
   download_url = 'https://github.com/B-UMMI/chewBBACA/archive/v2.0_pypi.tar.gz', 
   keywords = ['cgMLST', 'bacterial typing'],
-  classifiers = [],
+  classifiers = ['Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'],
   install_requires=['numpy>=1.13.1','scipy>=0.13.3','biopython>=1.70','plotly>=1.12.9','SPARQLWrapper>=1.8.0'],
 )

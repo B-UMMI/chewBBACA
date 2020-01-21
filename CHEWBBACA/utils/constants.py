dffaf6a142d0bd55ebcc914a1780d467d1370223
rfm <rfm.1991.cas@gmail.com>
2020-01-21 19:51:02 +0000

    Adding constants file to utils directory.

---
+++ CHEWBBACA/utils/constants.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+AUTHOR
+
+    Pedro Cerqueira
+    github: @pedrorvc
+
+    Rafael Mamede
+    github: @rfm-targa
+
+DESCRIPTION
+
+"""
+
+GENETIC_CODES = {11: 'The Bacterial, Archaeal and Plant Plastid Code'}
+
+CHEWIE_VERSIONS = ['2.1.0']
+
+
+

f330fee6a5ac8fa7faa68c0c022794bfba235dec
rfm <rfm.1991.cas@gmail.com>
2020-12-03 19:41:18 +0000

    Adding more tests for arguments passed to AlleleCall.

--- CHEWBBACA/tests/test_allele_call_sanity.py
+++ CHEWBBACA/tests/test_allele_call_sanity.py
@@ -22,45 +22,42 @@ args_template = ['chewBBACA.py', 'AlleleCall',
                  '-o', 'allelecall_results']
 
 
-#@pytest.mark.parametrize(
-#        'test_args, expected',
-#        [(['chewBBACA.py', 'AlleleCall',
-#           '-i', 'data/allelecall_data/test_genome',
-#           '-g', 'data/allelecall_data/sagalactiae_schema',
-#           '-o', 'allelecall_results'],
-#         'data/allelecall_data/test_results')
-#         ])
-#def test_allelecall_valid(test_args, expected):
-#    with patch.object(sys, 'argv', test_args):
-#        capture = py.io.StdCapture()
-#        chewBBACA.main()
-#        stdout, stderr = capture.reset()
-#
-#    # check text printed to stdout
-#    assert 'Writing output files' in stdout
-#
-#    # check output files
-#    for root, dirs, files in os.walk(test_args[7]):
-#        output_files = [os.path.join(root, file)
-#                        for file in files
-#                        if 'logging_info.txt' != file]
-#
-#    expected_files = [os.path.join(expected, file)
-#                      for file in os.listdir(expected)
-#                      if 'logging_info.txt' != file]
-#
-#    files = output_files + expected_files
-#    basename_dict = {}
-#    for f in files:
-#        basename = os.path.basename(f)
-#        basename_dict.setdefault(basename, []).append(f)
-#
-#    # assert that files in each pair are equal
-#    file_cmps = []
-#    for k, v in basename_dict.items():
-#        file_cmps.append(filecmp.cmp(v[0], v[1], shallow=False))
-#
-#    assert all(file_cmps) is True
+@pytest.mark.parametrize(
+        'test_args, expected',
+        [(args_template,
+         'data/allelecall_data/test_results')
+         ])
+def test_allelecall_valid(test_args, expected):
+    with patch.object(sys, 'argv', test_args):
+        capture = py.io.StdCapture()
+        chewBBACA.main()
+        stdout, stderr = capture.reset()
+
+    # check text printed to stdout
+    assert 'Writing output files' in stdout
+
+    # check output files
+    for root, dirs, files in os.walk(test_args[7]):
+        output_files = [os.path.join(root, file)
+                        for file in files
+                        if 'logging_info.txt' != file]
+
+    expected_files = [os.path.join(expected, file)
+                      for file in os.listdir(expected)
+                      if 'logging_info.txt' != file]
+
+    files = output_files + expected_files
+    basename_dict = {}
+    for f in files:
+        basename = os.path.basename(f)
+        basename_dict.setdefault(basename, []).append(f)
+
+    # assert that files in each pair are equal
+    file_cmps = []
+    for k, v in basename_dict.items():
+        file_cmps.append(filecmp.cmp(v[0], v[1], shallow=False))
+
+    assert all(file_cmps) is True
 
 
 @pytest.mark.parametrize(
@@ -80,7 +77,17 @@ args_template = ['chewBBACA.py', 'AlleleCall',
           'Must be equal or greater than 0.'),
          (args_template+['--l', 'sus'],
           '\nInvalid minimum sequence length value used to '
-          'create schema. Value must be a positive integer.')
+          'create schema. Value must be a positive integer.'),
+         (args_template+['--st', '-1'],
+          '\nInvalid size threshold value. '
+          'Must be contained in the [0.0, 1.0] interval.'),
+         (args_template+['--st', '1.1'],
+          '\nInvalid size threshold value. '
+          'Must be contained in the [0.0, 1.0] interval.'),
+         (args_template+['--st', 'sus'],
+          '\nInvalid size threshold value used to '
+          'create schema. Value must be None or a '
+          'positive float in the [0.0, 1.0] interval.'),
          ])
 def test_invalid_args(test_args, expected):
     with pytest.raises(SystemExit) as e:
@@ -89,5 +96,3 @@ def test_invalid_args(test_args, expected):
 
     assert e.type == SystemExit
     assert e.value.code == expected
-
-

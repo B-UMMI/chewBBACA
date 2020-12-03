4ca9311ac905f2063cf913eedaa74ca6afe49a56
rfm <rfm.1991.cas@gmail.com>
2020-12-03 13:08:31 +0000

    Adding test data for the AlelleCall process and test for valid input.

--- CHEWBBACA/tests/test_main_modules.py
+++ CHEWBBACA/tests/test_main_modules.py
@@ -6,8 +6,10 @@
 
 
 import py
+import os
 import sys
 import pytest
+import filecmp
 from unittest.mock import patch
 #from contextlib import nullcontext as does_not_raise
 
@@ -48,14 +50,37 @@ def test_modules_exit_codes(test_args, expected):
     assert e.value.code == expected
 
 
-#@pytest.mark.parametrize(
-#        'test_args, expected',
-#        [(['chewBBACA.py', 'AlleleCall', ], 0)
-#         ])
-#def test_allelecall(test_args, expected):
-#    with patch.object(sys, 'argv', test_args):
-#        capture = py.io.StdCapture()
-#        chewBBACA.main()
-#        stdout, stderr = capture.reset()
-#
-#    assert 'Select one of the following functions:' in stdout
+@pytest.mark.parametrize(
+        'test_args, expected',
+        [(['chewBBACA.py', 'AlleleCall',
+           '-i', 'data/allelecall_data/test_genome',
+           '-g', 'data/allelecall_data/sagalactiae_schema',
+           '-o', 'allelecall_results'],
+         'data/allelecall_data/test_results')
+         ])
+def test_allelecall_valid(test_args, expected):
+    with patch.object(sys, 'argv', test_args):
+        capture = py.io.StdCapture()
+        chewBBACA.main()
+        stdout, stderr = capture.reset()
+
+    # check text printed to stdout
+    assert 'Writing output files' in stdout
+
+    # check output files
+    for root, dirs, files in os.walk(expected):
+        output_files = [os.path.join(root, file)
+                        for file in files]
+
+    expected_files = [os.path.join(expected, file)
+                      for file in os.listdir(expected)]
+
+    files = output_files + expected_files
+    basename_dict = {}
+    for f in files:
+        basename = os.path.basename(f)
+        basename_dict.setdefault(basename, []).append(f)
+
+    # assert that files in each pair are equal
+    for k, v in basename_dict.items():
+        assert filecmp.cmp(v[0], v[1], shallow=False) is True

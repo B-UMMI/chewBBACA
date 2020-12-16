f40d87b1e69bc40269ec181802fde0f1598c7640
pedrorvc <pedrorvcerqueira@gmail.com>
2020-12-16 15:21:38 +0000

    Uncommented all test batches.

--- CHEWBBACA/tests/test_schema_evaluator_sanity.py
+++ CHEWBBACA/tests/test_schema_evaluator_sanity.py
@@ -57,32 +57,32 @@ def test_schemaEvaluator_valid(test_args, expected):
         chewBBACA.main()
         stdout, stderr = capture.reset()
 
-    # check if the report has been created 
-    # assert 'The report has been created.' in stdout
+    check if the report has been created 
+    assert 'The report has been created.' in stdout
 
-    # # check output HTML files
-    # output_html_files = [os.path.join(test_args[5], "html_files", file)
-    #                      for file in os.listdir(os.path.join(test_args[5], "html_files"))
-    #                      ]
-    # output_html_files.sort()
+    # check output HTML files
+    output_html_files = [os.path.join(test_args[5], "html_files", file)
+                         for file in os.listdir(os.path.join(test_args[5], "html_files"))
+                         ]
+    output_html_files.sort()
 
-    # expected_html_files = [os.path.join(expected, "html_files", file)
-    #                        for file in os.listdir(os.path.join(expected, "html_files"))
-    #                        ]
-    # expected_html_files.sort()
+    expected_html_files = [os.path.join(expected, "html_files", file)
+                           for file in os.listdir(os.path.join(expected, "html_files"))
+                           ]
+    expected_html_files.sort()
 
-    # html_files = output_html_files + expected_html_files
-    # basename_html_dict = {}
-    # for f1 in html_files:
-    #     basename1 = os.path.basename(f1)
-    #     basename_html_dict.setdefault(basename1, []).append(f1)
+    html_files = output_html_files + expected_html_files
+    basename_html_dict = {}
+    for f1 in html_files:
+        basename1 = os.path.basename(f1)
+        basename_html_dict.setdefault(basename1, []).append(f1)
 
-    # # assert that files in each pair are equal
-    # file_cmps_html = []
-    # for k, v in basename_html_dict.items():
-    #     file_cmps_html.append(filecmp.cmp(v[0], v[1], shallow=False))
+    # assert that files in each pair are equal
+    file_cmps_html = []
+    for k, v in basename_html_dict.items():
+        file_cmps_html.append(filecmp.cmp(v[0], v[1], shallow=False))
 
-    # assert all(file_cmps_html) is True
+    assert all(file_cmps_html) is True
 
     # check output MAIN files
     output_main_files = [os.path.join(test_args[5], "SchemaEvaluator_pre_computed_data", file)
@@ -102,72 +102,56 @@ def test_schemaEvaluator_valid(test_args, expected):
         basename_main_dict.setdefault(basename2, []).append(f2)
 
     # assert that files in each pair are equal
-    # print(list(basename_main_dict.values())[3][0])
-    # print(list(basename_main_dict.values())[3][1])
-    
-    # print(list(basename_main_dict.items()))
     file_cmps_main = []
     for k2, v2 in basename_main_dict.items():
         file_cmps_main.append(filecmp.cmp(v2[0], v2[1], shallow=False))
 
-    # with open(list(basename_main_dict.values())[4][0], "r") as o:
-    #     out = o.read()
-
-    # print(out)
-
-    # with open(list(basename_main_dict.values())[4][1], "r") as e:
-    #     exc = e.read()
-
-    # print(exc)
-
-    print(file_cmps_main)
-
     assert all(file_cmps_main) is True
 
     # check output PROTEIN files
-    # output_prot_files = [os.path.join(test_args[5], "SchemaEvaluator_pre_computed_data", "prot_files", file)
-    #                      for file in os.listdir(os.path.join(test_args[5], "SchemaEvaluator_pre_computed_data", "prot_files"))
-    #                      if "exceptions" != file]
-    # output_prot_files.sort()
+    output_prot_files = [os.path.join(test_args[5], "SchemaEvaluator_pre_computed_data", "prot_files", file)
+                         for file in os.listdir(os.path.join(test_args[5], "SchemaEvaluator_pre_computed_data", "prot_files"))
+                         if "exceptions" != file]
+    output_prot_files.sort()
 
-    # expected_prot_files = [os.path.join(expected, "SchemaEvaluator_pre_computed_data", "prot_files", file)
-    #                        for file in os.listdir(os.path.join(expected, "SchemaEvaluator_pre_computed_data", "prot_files"))
-    #                        if "exceptions" != file]
-    # expected_prot_files.sort()
+    expected_prot_files = [os.path.join(expected, "SchemaEvaluator_pre_computed_data", "prot_files", file)
+                           for file in os.listdir(os.path.join(expected, "SchemaEvaluator_pre_computed_data", "prot_files"))
+                           if "exceptions" != file]
+    expected_prot_files.sort()
 
-    # prot_files = output_prot_files + expected_prot_files
-    # basename_prot_dict = {}
-    # for f in prot_files:
-    #     basename3 = os.path.basename(f)
-    #     basename_prot_dict.setdefault(basename3, []).append(f)
+    prot_files = output_prot_files + expected_prot_files
+    basename_prot_dict = {}
+    for f in prot_files:
+        basename3 = os.path.basename(f)
+        basename_prot_dict.setdefault(basename3, []).append(f)
 
-    # # assert that files in each pair are equal
-    # file_cmps_prot = []
-    # for k3, v3 in basename_prot_dict.items():
-    #     file_cmps_prot.append(filecmp.cmp(v3[0], v3[1], shallow=False))
+    # assert that files in each pair are equal
+    file_cmps_prot = []
+    for k3, v3 in basename_prot_dict.items():
+        file_cmps_prot.append(filecmp.cmp(v3[0], v3[1], shallow=False))
 
-    # assert all(file_cmps_prot) is True
+    assert all(file_cmps_prot) is True
 
-    # # check output EXCEPTION files
-    # output_exc_files = [os.path.join(test_args[5], "SchemaEvaluator_pre_computed_data", "prot_files", "exceptions", file)
-    #                      for file in os.listdir(os.path.join(test_args[5], "SchemaEvaluator_pre_computed_data", "prot_files", "exceptions"))
-    #                     ]
-    # output_exc_files.sort()
+    # check output EXCEPTION files
+    output_exc_files = [os.path.join(test_args[5], "SchemaEvaluator_pre_computed_data", "prot_files", "exceptions", file)
+                         for file in os.listdir(os.path.join(test_args[5], "SchemaEvaluator_pre_computed_data", "prot_files", "exceptions"))
+                        ]
+    output_exc_files.sort()
 
-    # expected_exc_files = [os.path.join(expected, "SchemaEvaluator_pre_computed_data", "prot_files", "exceptions", file)
-    #                        for file in os.listdir(os.path.join(expected, "SchemaEvaluator_pre_computed_data", "prot_files", "exceptions"))
-    #                       ]
-    # expected_exc_files.sort()
+    expected_exc_files = [os.path.join(expected, "SchemaEvaluator_pre_computed_data", "prot_files", "exceptions", file)
+                           for file in os.listdir(os.path.join(expected, "SchemaEvaluator_pre_computed_data", "prot_files", "exceptions"))
+                          ]
+    expected_exc_files.sort()
 
-    # exc_files = output_exc_files + expected_exc_files
-    # basename_exc_dict = {}
-    # for f in exc_files:
-    #     basename4 = os.path.basename(f)
-    #     basename_exc_dict.setdefault(basename4, []).append(f)
+    exc_files = output_exc_files + expected_exc_files
+    basename_exc_dict = {}
+    for f in exc_files:
+        basename4 = os.path.basename(f)
+        basename_exc_dict.setdefault(basename4, []).append(f)
 
-    # # assert that files in each pair are equal
-    # file_cmps_exc = []
-    # for k4, v4 in basename_exc_dict.items():
-    #     file_cmps_exc.append(filecmp.cmp(v4[0], v4[1], shallow=False))
+    # assert that files in each pair are equal
+    file_cmps_exc = []
+    for k4, v4 in basename_exc_dict.items():
+        file_cmps_exc.append(filecmp.cmp(v4[0], v4[1], shallow=False))
 
-    # assert all(file_cmps_exc) is True
+    assert all(file_cmps_exc) is True

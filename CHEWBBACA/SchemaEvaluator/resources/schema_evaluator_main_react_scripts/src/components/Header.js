d2dd82552ba8932d444e0a3f0d127df72d5fa2b4
pedrorvc <pedrorvcerqueira@gmail.com>
2021-04-23 17:24:08 +0100

    Added the Sequence Logo component. A component to zoom and pan the logo was also added.

--- CHEWBBACA/SchemaEvaluator/resources/schema_evaluator_main_react_scripts/src/components/Header.js
+++ CHEWBBACA/SchemaEvaluator/resources/schema_evaluator_main_react_scripts/src/components/Header.js
@@ -25,7 +25,7 @@ export default function Header() {
   By selecting a locus the following will be displayed:
   - 2 charts (histogram and scatterplot) containing an **analysis of the allele sizes**;
   - a table with **summary statistics** of the alleles;
-  - a **Neighbor Joining tree** built by clustal based on the mafft alignment;
+  - a **Neighbor Joining tree** based on the mafft alignment;
   - a **multiple sequence alignment** of the alleles produced by mafft.
   `;
 

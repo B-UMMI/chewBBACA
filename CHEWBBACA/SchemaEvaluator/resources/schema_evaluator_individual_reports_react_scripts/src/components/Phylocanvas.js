e7e7cd5213120bf6b845d9ce5b16e72503ed50bd
rfm-targa <rmamede@medicina.ulisboa.pt>
2022-11-16 12:22:19 +0000

    Removed Python 3.11 from tests.

--- CHEWBBACA/SchemaEvaluator/resources/schema_evaluator_individual_reports_react_scripts/src/components/Phylocanvas.js
+++ CHEWBBACA/SchemaEvaluator/resources/schema_evaluator_individual_reports_react_scripts/src/components/Phylocanvas.js
@@ -19,7 +19,7 @@ export class PhylogeneticTree extends React.Component {
   
       this.tree = null;
     }
-  
+
     setTreeAttributes = () => {
       // Set the tree type to rectangular
       this.tree.setTreeType(this.props.treeType);
@@ -28,22 +28,22 @@ export class PhylogeneticTree extends React.Component {
       this.tree.resizeToContainer();
       this.tree.draw();
     };
-  
+
     componentDidMount = () => {
       this.tree = Phylocanvas.createTree(this.node, { nodeAlign: true });
       let newickTree = this.props.newickString;
-  
+
       // Load the newick tree from props
       this.tree.load(newickTree);
       this.setTreeAttributes();
       // tree.load("((_R_CC0067_NODE_1_length_10197_cov_734.723715_pilon:0.0006430160208146113,(CC0061_k77_1_flag_1_multi_4641.2458_len_10267_pilon:0.00953411150529446,_R_CC0116_NODE_1_length_10196_cov_675.686135_pilon:0.004676216709352007)56:0.003523710625083518)100:0.5306604729360941,Spike_NODE_2_length_10199_cov_229.021834_pilon:0.29987078286758695,_R_Spike_NODE_1_length_10319_cov_2021.808436_pilon:0.2754770121464598);");
     };
-  
+
     render() {
       if (this.node) {
         this.setTreeAttributes();
       }
-  
+
       return (
         <div style={{ width: "100%", zIndex: "1" }}>
           <div
@@ -54,4 +54,3 @@ export class PhylogeneticTree extends React.Component {
       );
     }
   }
-  

0d6db54e1eeebf52b1195fc8ee7d1736dea49663
rfm-targa <rmamede@medicina.ulisboa.pt>
2022-12-21 13:54:24 +0000

    Added comments, more info stdout and adjusted plot layout.

--- CHEWBBACA/ExtractCgMLST/Extract_cgAlleles.py
+++ CHEWBBACA/ExtractCgMLST/Extract_cgAlleles.py
@@ -50,10 +50,12 @@ from plotly.offline import plot
 import plotly.graph_objects as go
 
 try:
-    from utils import (iterables_manipulation as im,
+    from utils import (file_operations as fo,
+                       iterables_manipulation as im,
                        constants as ct)
 except ModuleNotFoundError:
-    from CHEWBBACA.utils import (iterables_manipulation as im,
+    from CHEWBBACA.utils import (file_operations as fo,
+                                 iterables_manipulation as im,
                                  constants as ct)
 
 
@@ -106,9 +108,6 @@ def remove_genomes(matrix, genomesToRemove):
     # create new matrix without rows that matched any genome to remove
     pruned_matrix = matrix.loc[~ to_remove_bool]
 
-    print('Removed {0} profiles that matched list of '
-          'genomes to exclude.'.format(len(genomesToRemove)))
-
     return pruned_matrix
 
 
@@ -142,10 +141,6 @@ def above_threshold(column, column_length, threshold):
     return (np.sum(column) / column_length) >= threshold
 
 
-# matrix = presence_absence
-# sorted_genomes = sorted_genomes
-# threshold = t
-# step = step
 def compute_cgMLST(matrix, sorted_genomes, threshold, step):
     """Prune allele call results based on presence/absence threshold.
 
@@ -179,7 +174,6 @@ def compute_cgMLST(matrix, sorted_genomes, threshold, step):
     # determine genes at or above threshold
     cgMLST_size = {}
     for i in im.inclusive_range(1, len(sorted_genomes), step):
-        print(i)
         # get subdataframe for current genomes
         current_df = matrix.loc[sorted_genomes[:i]]
         pa_rows, _ = current_df.shape
@@ -188,6 +182,7 @@ def compute_cgMLST(matrix, sorted_genomes, threshold, step):
         above = current_df.columns[is_above_threshold]
         cgMLST_size[pa_rows] = len(above)
         pruned_df = current_df.loc[:, above]
+        print('\r', 'Computed for...{0} genomes.'.format(i), end='')
 
     # return last df with cgMLST for all genomes
     return [pruned_df, cgMLST_size]
@@ -216,10 +211,10 @@ def presAbs(matrix, output_directory):
     """
     presence_absence = matrix.apply(binarize_matrix)
 
-    pa_path = os.path.join(output_directory, 'Presence_Absence.tsv')
+    pa_path = fo.join_paths(output_directory, ['Presence_Absence.tsv'])
     presence_absence.to_csv(pa_path, sep='\t')
 
-    return presence_absence
+    return [presence_absence, pa_path]
 
 
 def missing_data_table(presence_absence):
@@ -253,12 +248,6 @@ def missing_data_table(presence_absence):
     return missing_data_df
 
 
-# input_file = '/home/rmamede/Desktop/chewBBACA_tutorial_update/chewBBACA_tutorial/expected_results/Allele_calling/results32_wgMLST/results_alleles.tsv'
-# output_directory = '/home/rmamede/Desktop/chewBBACA_tutorial_update/chewBBACA_tutorial/expected_results/Allele_calling/results32_wgMLST/cgMLST_test'
-# genesToRemove = ['GCA-000007265-protein1', 'GCA-000007265-protein10']
-# genomesToRemove = ['GCA_000196055', 'GCA_000007265']
-# thresholds = [0.95, 0.99, 1]
-# step = 1
 def main(input_file, output_directory, threshold, step,
          genes2remove, genomes2remove):
     """Determine the cgMLST based on allele calling results.
@@ -290,33 +279,35 @@ def main(input_file, output_directory, threshold, step,
     - Path to a TSV file with the information about
       missing data per genome.
     """
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
+    fo.create_directory(output_directory)
+
+    # import matrix with allelic profiles
+    matrix = pd.read_csv(input_file, header=0, index_col=0,
+                         sep='\t', low_memory=False)
+
+    total_genomes, total_loci = matrix.shape
+    print('Input matrix has {0} profiles for {1} '
+          'loci.'.format(total_genomes, total_loci))
 
     # read list of genes and list of genomes to exclude
     genesToRemove = []
     if genes2remove:
-        with open(genes2remove, 'r') as gr:
-            genesToRemove = gr.read().splitlines()
+        genesToRemove = fo.read_lines(genes2remove)
+    print('{0} loci to exclude.'.format(len(genesToRemove)))
     genomesToRemove = []
     if genomes2remove:
-        with open(genomes2remove, 'r') as gr:
-            genomesToRemove = gr.read().splitlines()
+        genomesToRemove = fo.read_lines(genomes2remove)
+    print('{0} genomes to exclude.'.format(len(genomesToRemove)))
 
     if threshold is None:
         cgMLST_thresholds = ct.CGMLST_THRESHOLDS
     else:
         cgMLST_thresholds = threshold
 
-    # import matrix with allelic profiles
-    matrix = pd.read_csv(input_file, header=0, index_col=0,
-                         sep='\t', low_memory=False)
-
-    total_loci = len(matrix.columns)
-
     # remove genomes
     if len(genomesToRemove) > 0:
         matrix = remove_genomes(matrix, genomesToRemove)
+        total_genomes -= len(genomesToRemove)
 
     # remove genes
     if len(genesToRemove) > 0:
@@ -324,19 +315,20 @@ def main(input_file, output_directory, threshold, step,
         total_loci -= len(genesToRemove)
 
     # mask missing data
-    print('Masking missing data...', end='')
+    print('Masking matrix with {0} profiles for {1} '
+          'loci...'.format(total_genomes, total_loci))
     masked_matrix = matrix.apply(im.replace_chars)
-    print('done.')
+    print('Masked {0} profiles.'.format(total_genomes))
 
     # build presence/absence matrix
-    print('Building presence and absence matrix...', end='')
-    presence_absence = presAbs(masked_matrix, output_directory)
-    print('done.')
+    print('Building presence-absence matrix...')
+    pa_matrix, pa_outfile = presAbs(masked_matrix, output_directory)
+    print('Presence-absence matrix saved to {0}'.format(pa_outfile))
 
     # count number of missing data per genome
-    print('Determining missing data per genome...', end='')
-    missing_data_df = missing_data_table(presence_absence)
-    print('done.')
+    print('Determining missing data per genome...')
+    missing_data_df = missing_data_table(pa_matrix)
+    missing_stats = missing_data_df['missing'].describe()
 
     # sort genomes based on order of decreasing missing data
     missing_data_df = missing_data_df.sort_values('missing', ascending=True)
@@ -345,59 +337,69 @@ def main(input_file, output_directory, threshold, step,
     # write table with missing data stats
     mdata_path = os.path.join(output_directory, 'mdata_stats.tsv')
     missing_data_df.to_csv(mdata_path, sep='\t', index=False)
+    print('Missing data table saved to {0}'.format(mdata_path))
 
     # compute cgMLST
     line_traces = []
     for i, t in enumerate(cgMLST_thresholds):
-        print('Determining genes in the core genome at {0}...'.format(t), end='')
-        gene_pruned, cgMLST_genes = compute_cgMLST(presence_absence,
+        print('Determining cgMLST for loci presence threshold of {0}...'.format(t))
+        gene_pruned, cgMLST_genes = compute_cgMLST(pa_matrix,
                                                    sorted_genomes,
                                                    t,
                                                    step)
-        print('done.')
+
+        retained = len(gene_pruned.columns)
+        print('\ncgMLST for loci presence threshold of {0}% composed '
+              'of {1}/{2} genes.'.format(t, retained, total_loci))
 
         # write cgMLST matrix
         cgmlst_path = os.path.join(output_directory, 'cgMLST{0}.tsv'.format(int(t*100)))
         gene_pruned.to_csv(cgmlst_path, sep='\t')
+        print('cgMLST profiles saved to {0}'.format(cgmlst_path))
 
         # write genes in cgMLST to file
         loci_path = os.path.join(output_directory, 'cgMLSTschema{0}.txt'.format(int(t*100)))
         pd.Series(list(gene_pruned.columns.values)).to_csv(loci_path,
                                                            index=False,
                                                            header=False)
-
-        retained = len(gene_pruned.columns)
-        print('\nCore genome at {0}% composed of {1}/{2} genes.'
-              ''.format(int(t*100), retained, total_loci))
+        print('List of loci in the cgMLST saved to {0}'.format(loci_path))
 
         # create line trace
-        cgMLST_trace = go.Scatter(x=list(cgMLST_genes.keys()),
-                                  y=list(cgMLST_genes.values()),
-                                  mode='lines+markers',
-                                  name='cgMLST{0}'.format(int(t*100)),
-                                  hovertemplate=('%{y}'))
+        cgMLST_trace = go.Scattergl(x=list(cgMLST_genes.keys()),
+                                    y=list(cgMLST_genes.values()),
+                                    mode='lines',
+                                    name='cgMLST{0}'.format(int(t*100)),
+                                    hovertemplate=('%{y}'))
         line_traces.append(cgMLST_trace)
 
     # create trace with present loci
     present = [total_loci-i for i in missing_data_df['missing']]
     genomes_index = list(range(1, len(present)+1))
-    miss_trace = go.Scatter(x=genomes_index,
-                            y=present,
-                            mode='lines+markers',
-                            name='Present in genome',
-                            line=dict(color='#000000'),
-                            hovertemplate=('%{y}<br>'
-                                           'Genome: %{text}<br>'),
-                            text=sorted_genomes)
+    miss_trace = go.Scattergl(x=genomes_index,
+                              y=present,
+                              mode='lines',
+                              name='Present in genome',
+                              line=dict(color='#000000'),
+                              hovertemplate=('%{y}<br>'
+                                             'Genome: %{text}<br>'),
+                              text=sorted_genomes)
     line_traces.append(miss_trace)
 
     fig = go.Figure(data=line_traces)
-    fig.update_layout(title={'text': 'Number of loci in cgMLST', 'font_size': 30},
+    fig.update_layout(title={'text': 'Number of loci in cgMLST',
+                             'font_size': 30},
                       xaxis_title='Number of genomes',
                       yaxis_title='Number of loci',
                       template='simple_white',
                       hovermode='x')
-    fig.update_xaxes(range=[0,len(sorted_genomes)], tickfont=dict(size=18), titlefont=dict(size=20), showgrid=True)
-    fig.update_yaxes(tickfont=dict(size=18), titlefont=dict(size=20), showgrid=True)
+    fig.update_xaxes(range=[0, len(sorted_genomes)],
+                     tickfont=dict(size=18),
+                     titlefont=dict(size=20),
+                     showgrid=True)
+    fig.update_yaxes(tickfont=dict(size=18),
+                     titlefont=dict(size=20),
+                     showgrid=True)
     output_html = os.path.join(output_directory, 'cgMLST.html')
-    plot(fig, filename=output_html)
+    plot(fig, filename=output_html, auto_open=False)
+    print('HTML file with cgMLST per loci presence threshold '
+          'and per step saved to {0}'.format(output_html))

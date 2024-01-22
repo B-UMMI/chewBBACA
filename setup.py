35c6a7095e61209289ab3dd54e16aec256b63807
rfm-targa <rmamede@medicina.ulisboa.pt>
2024-01-22 17:36:54 +0000

    Added pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,45 +1,46 @@
 from setuptools import setup, find_namespace_packages
 
-import CHEWBBACA
+#import CHEWBBACA
 
-VERSION = CHEWBBACA.__version__
+#VERSION = CHEWBBACA.__version__
 
 packages = find_namespace_packages(
   exclude=[
-    "CHEWBBACA.tests*"
+    "CHEWBBACA.tests*",
+    "CHEWBBACA.docs*"
   ]
 )
 
-install_requires = (
-  'numpy~=1.24.3',
-  'scipy~=1.10.1',
-  'biopython>=1.79',
-  'plotly>=5.8.0',
-  'SPARQLWrapper>=2.0.0',
-  'pandas>=1.5.1,<2.1',
-  'requests>=2.27.1',
-  'pyrodigal>=3.0.0'
-)
+# install_requires = (
+#   'numpy~=1.24.3',
+#   'scipy~=1.10.1',
+#   'biopython>=1.79',
+#   'plotly>=5.8.0',
+#   'SPARQLWrapper>=2.0.0',
+#   'pandas>=1.5.1,<2.1',
+#   'requests>=2.27.1',
+#   'pyrodigal>=3.0.0'
+# )
 
 setup(
-  name = 'chewBBACA',
+  # name = 'chewBBACA',
   packages=packages,
-  version = VERSION,
-  description = 'A complete suite for gene-by-gene schema creation and strain identification.',
-  author = 'Rafael Mamede, Pedro Cerqueira, Mickael Silva, João Carriço, Mário Ramirez',
-  author_email = 'imm-bioinfo@medicina.ulisboa.pt',
-  url = 'https://github.com/B-UMMI/chewBBACA', 
-  keywords = ['cgMLST', 'bacterial typing', 'nomenclature server'],
-  install_requires = install_requires,
-  python_requires = '>=3.7',
+  # version = VERSION,
+  # description = 'A complete suite for gene-by-gene schema creation and strain identification.',
+  # author = 'Rafael Mamede, Pedro Cerqueira, Mickael Silva, João Carriço, Mário Ramirez',
+  # author_email = 'imm-bioinfo@medicina.ulisboa.pt',
+  # url = 'https://github.com/B-UMMI/chewBBACA', 
+  # keywords = ['cgMLST', 'bacterial typing', 'nomenclature server'],
+  # install_requires = install_requires,
+  # python_requires = '>=3.7',
   include_package_data = True,
-  entry_points={'console_scripts': ["chewBBACA.py = CHEWBBACA.chewBBACA:main",
-                                    "chewie = CHEWBBACA.chewBBACA:main"]
-                },
-  project_urls={
-    'Documentation': 'https://chewbbaca.readthedocs.io/en/latest/index.html',
-    'Source': 'https://github.com/B-UMMI/chewBBACA',
-  },
-  maintainer_email='imm-bioinfo@medicina.ulisboa.pt',
-  maintainer='UMMI'
+  # entry_points={'console_scripts': ["chewBBACA.py = CHEWBBACA.chewBBACA:main",
+  #                                   "chewie = CHEWBBACA.chewBBACA:main"]
+  #               },
+  # project_urls={
+  #   'Documentation': 'https://chewbbaca.readthedocs.io/en/latest/index.html',
+  #   'Source': 'https://github.com/B-UMMI/chewBBACA',
+  # },
+  # maintainer_email='imm-bioinfo@medicina.ulisboa.pt',
+  # maintainer='UMMI'
 )

b9cef5180bf90275a86de58177f0be4efbcc811e
rfm-targa <rmamede@medicina.ulisboa.pt>
2024-01-22 14:00:53 +0000

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -4,27 +4,40 @@ import CHEWBBACA
 
 VERSION = CHEWBBACA.__version__
 
-packages = find_namespace_packages(include=['CHEWBBACA'])
+packages = find_namespace_packages(include=['CHEWBBACA*'])
+exclude_package_data = {"CHEWBBACA": ["tests*"]}
+
+install_requires = (
+  'numpy~=1.24.3',
+  'scipy~=1.10.1',
+  'biopython>=1.79',
+  'plotly>=5.8.0',
+  'SPARQLWrapper>=2.0.0',
+  'pandas>=1.5.1,<2.1',
+  'requests>=2.27.1',
+  'pyrodigal>=3.0.0'
+)
 
 setup(
   name = 'chewBBACA',
-  # packages = ['CHEWBBACA', 'CHEWBBACA.AlleleCall', 'CHEWBBACA.CreateSchema',
-  #             'CHEWBBACA.utils', 'CHEWBBACA.SchemaEvaluator', 'CHEWBBACA.PrepExternalSchema',
-  #             'CHEWBBACA.CHEWBBACA_NS', 'CHEWBBACA.UniprotFinder', 'CHEWBBACA.ExtractCgMLST',
-  #             'CHEWBBACA.AlleleCallEvaluator'],
   packages=packages,
+  exclude_package_data=exclude_package_data,
   version = VERSION,
   description = 'A complete suite for gene-by-gene schema creation and strain identification.',
   author = 'Rafael Mamede, Pedro Cerqueira, Mickael Silva, João Carriço, Mário Ramirez',
   author_email = 'imm-bioinfo@medicina.ulisboa.pt',
   url = 'https://github.com/B-UMMI/chewBBACA', 
   keywords = ['cgMLST', 'bacterial typing', 'nomenclature server'],
-  install_requires = ['numpy~=1.24.3', 'scipy~=1.10.1', 'biopython>=1.79',
-                      'plotly>=5.8.0', 'SPARQLWrapper>=2.0.0', 'pandas>=1.5.1,<2.1',
-                      'requests>=2.27.1', 'pyrodigal>=3.0.0'],
+  install_requires = install_requires,
   python_requires = '>=3.7',
   include_package_data = True,
   entry_points={'console_scripts': ["chewBBACA.py = CHEWBBACA.chewBBACA:main",
                                     "chewie = CHEWBBACA.chewBBACA:main"]
                 }
+  project_urls={
+    'Documentation': 'https://chewbbaca.readthedocs.io/en/latest/index.html',
+    'Source': 'https://github.com/B-UMMI/chewBBACA',
+  },
+  maintainer_email='imm-bioinfo@medicina.ulisboa.pt',
+  maintainer='UMMI'
 )

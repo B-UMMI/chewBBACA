277ffc79d455b2a8c9a558165a4c36be05cced10
rfm-targa <rmamede@medicina.ulisboa.pt>
2024-05-23 14:11:37 +0100

    Fixed issue in RemoveGenes module where it would not get first ID in loci list.

--- CHEWBBACA/utils/remove_genes.py
+++ CHEWBBACA/utils/remove_genes.py
@@ -17,50 +17,50 @@ import csv
 import pandas as pd
 
 try:
-    from utils import file_operations as fo
+	from utils import file_operations as fo
 except ModuleNotFoundError:
-    from CHEWBBACA.utils import file_operations as fo
+	from CHEWBBACA.utils import file_operations as fo
 
 
 def main(input_file, genes_list, output_file, inverse):
-    """Remove loci from allele calling results.
+	"""Remove loci from allele calling results.
 
-    Parameters
-    ----------
-    input_file : str
-        Path to a TSV file that contains allelic profiles
-        determined by the AlleleCall module.
-    genes_list : str
-        Path to a file with a list of loci to keep or remove.
-    output_file : str
-        Path to the output file.
-    inverse : bool
-        Keep the loci included in `genes_list` and remove the
-        rest instead.
-    """
-    # Read genes list
-    with open(genes_list, 'r') as infile:
-        genes_list = list(csv.reader(infile, delimiter='\t'))
-        genes_list = [g[0] for g in genes_list[1:]]
+	Parameters
+	----------
+	input_file : str
+		Path to a TSV file that contains allelic profiles
+		determined by the AlleleCall module.
+	genes_list : str
+		Path to a file with a list of loci to keep or remove.
+	output_file : str
+		Path to the output file.
+	inverse : bool
+		Keep the loci included in `genes_list` and remove the
+		rest instead.
+	"""
+	# Read genes list
+	with open(genes_list, 'r') as infile:
+		genes_list = list(csv.reader(infile, delimiter='\t'))
+		genes_list = [g[0] for g in genes_list]
 
-    # Get list of loci in allele call results
-    loci = fo.read_lines(input_file, strip=True, num_lines=1)
-    loci = loci[0].split('\t')[1:]
-    print('Total loci: {0}'.format(len(loci)))
+	# Get list of loci in allele call results
+	loci = fo.read_lines(input_file, strip=True, num_lines=1)
+	loci = loci[0].split('\t')[1:]
+	print('Total loci: {0}'.format(len(loci)))
 
-    if inverse is True:
-        columns_to_keep = [g for g in loci if g in genes_list]
-    else:
-        columns_to_keep = [g for g in loci if g not in genes_list]
+	if inverse is True:
+		columns_to_keep = [g for g in loci if g in genes_list]
+	else:
+		columns_to_keep = [g for g in loci if g not in genes_list]
 
-    columns_to_remove = (len(loci)) - len(columns_to_keep)
-    print('Loci to remove: {0}'.format(columns_to_remove))
+	columns_to_remove = (len(loci)) - len(columns_to_keep)
+	print('Loci to remove: {0}'.format(columns_to_remove))
 
-    # Include first column with sample ids
-    columns_to_keep = ['FILE'] + columns_to_keep
-    df = pd.read_csv(input_file, usecols=columns_to_keep,
-                     sep='\t', dtype=str)
+	# Include first column with sample ids
+	columns_to_keep = ['FILE'] + columns_to_keep
+	df = pd.read_csv(input_file, usecols=columns_to_keep,
+					 sep='\t', dtype=str)
 
-    # Save dataframe to file
-    df.to_csv(output_file, header=True, sep='\t', index=False)
+	# Save dataframe to file
+	df.to_csv(output_file, header=True, sep='\t', index=False)
 

f974d98c2f9001122caecaef524e9ba71d98c910
rfm-targa <rmamede@medicina.ulisboa.pt>
2024-07-11 19:07:39 +0100

    Changed version.

--- CHEWBBACA/docs/conf.py
+++ CHEWBBACA/docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, UMMI'
 author = 'Rafael Mamede'
 
 # The full version, including alpha/beta/rc tags
-release = '3.3.8'
+release = '3.3.9'
 
 # -- General configuration ---------------------------------------------------
 
